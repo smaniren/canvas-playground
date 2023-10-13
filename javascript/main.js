d5deec453458986c571c3a59cd1f0eca38f72fc0
smaniren <renzosmania@hotmail.com>
2023-10-13 13:55:38 +0200

    13.10.2023

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,70 @@
 //setup
-const canvas = document.getElementById('canvas1')
-const ctx = canvas.getContext('2d')
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-console.log(ctx)
+ctx.fillStyle = "white";
 
-ctx.fillStyle = 'white'
-ctx.fillRect(50, 50, 50 ,50;)
-
-class Particle{
-
+class Particle {
+  constructor(effect) {
+    this.effect = effect;
+    this.radius = Math.random() * 40 + 10;
+    this.x =
+      this.radius + Math.random() * (this.effect.width - this.radius * 2);
+    this.y =
+      this.radius + Math.random() * (this.effect.height - this.radius * 2);
+    this.vx = Math.random() * 3;
+    this.vy = Math.random() * 3;
+  }
+  draw(context) {
+    context.fillStyle = "hsl(" + this.x * 0.5 + ",100%,50%)";
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.fill();
+    context.stroke();
+  }
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+    if (this.x > this.effect.width - this.radius || this.x < this.radius) {
+      this.vx *= -1;
+    }
+    if (this.y > this.effect.height - this.radius || this.y < this.radius) {
+      this.vy *= -1;
+    }
+  }
 }
 
-class Effect{
-
+class Effect {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.particles = [];
+    this.numberOfParticles = 200;
+    this.createParticles();
+  }
+  createParticles() {
+    for (let i = 0; i < this.numberOfParticles; i++) {
+      this.particles.push(new Particle(this));
+    }
+  }
+  handleParticles(context) {
+    this.particles.forEach((particle) => {
+      particle.draw(context);
+      particle.update();
+    });
+  }
 }
 
-function animate(){
+const effect = new Effect(canvas);
+effect.handleParticles(ctx);
 
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  effect.handleParticles(ctx);
+  requestAnimationFrame(animate);
 }
+
+animate();
