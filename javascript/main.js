77abde4c1632b343c3a2e997f0f4ff187f05e709
smaniren <renzosmania@hotmail.com>
2023-10-13 12:55:17 +0200

    13.10.2023

---
+++ javascript/main.js
@@ -0,0 +1,21 @@
+//setup
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+console.log(ctx)
+
+ctx.fillStyle = 'white'
+ctx.fillRect(50, 50, 50 ,50;)
+
+class Particle{
+
+}
+
+class Effect{
+
+}
+
+function animate(){
+
+}
