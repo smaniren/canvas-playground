8fba935954ed6ed8ff83f3543aafa3ef9c09bd45
smaniren <renzosmania@hotmail.com>
2023-10-20 10:43:31 +0200

    20.10.2023

--- javascript/main.js
+++ javascript/main.js
@@ -3,25 +3,30 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.fillStyle = "white";
+
+const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+gradient.addColorStop(0, "white");
+gradient.addColorStop(0.5, "magenta");
+gradient.addColorStop(1, "blue");
+ctx.fillStyle = gradient;
+ctx.strokeStyle = "white";
 
 class Particle {
   constructor(effect) {
     this.effect = effect;
-    this.radius = Math.random() * 40 + 10;
+    this.radius = Math.random() * 5 + 2;
     this.x =
       this.radius + Math.random() * (this.effect.width - this.radius * 2);
     this.y =
       this.radius + Math.random() * (this.effect.height - this.radius * 2);
-    this.vx = Math.random() * 3;
-    this.vy = Math.random() * 3;
+    this.vx = Math.random() * 1.5 - 0.5;
+    this.vy = Math.random() * 1.5 - 0.5;
   }
   draw(context) {
-    context.fillStyle = "hsl(" + this.x * 0.5 + ",100%,50%)";
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     context.fill();
-    context.stroke();
+    //context.stroke();
   }
   update() {
     this.x += this.vx;
@@ -50,10 +55,31 @@ class Effect {
     }
   }
   handleParticles(context) {
+    this.connectParticles(context);
     this.particles.forEach((particle) => {
       particle.draw(context);
       particle.update();
     });
+  }
+  connectParticles(context) {
+    const maxDistance = 75;
+    for (let a = 0; a < this.particles.length; a++) {
+      for (let b = a; b < this.particles.length; b++) {
+        const dx = this.particles[a].x - this.particles[b].x;
+        const dy = this.particles[a].y - this.particles[b].y;
+        const distance = Math.hypot(dx, dy);
+        if (distance < maxDistance) {
+          const opacity = 1 - distance / maxDistance;
+          context.save();
+          context.globalAlpha = opacity;
+          context.beginPath();
+          context.moveTo(this.particles[a].x, this.particles[a].y);
+          context.lineTo(this.particles[b].x, this.particles[b].y);
+          context.stroke();
+          context.restore();
+        }
+      }
+    }
   }
 }
 
